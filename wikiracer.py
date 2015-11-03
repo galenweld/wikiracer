c5165b08d9b995b2ccb6bb8ea3a75f21415a3a4b
Galen Weld <galen.weld@gmail.com>
2015-11-03 13:29:02 -0500

    wrapped wikiracer in navigate function

--- wikiracer.py
+++ wikiracer.py
@@ -2,35 +2,35 @@ import operator
 import page
 import tf_idf
 
-origin_title = "University of Washington"
-destination_title = "Seattle"
+def navigate(origin_title, destination_title):
+	origin = page.Page(origin_title)
+	destination = page.Page(destination_title)
 
-origin = page.Page(origin_title)
-destination = page.Page(destination_title)
+	current_page = origin
+	print "starting at " + current_page.title()
 
-current_page = origin
-print "starting at " + current_page.title()
+	while current_page != destination:
+		possibilities = []
+		for title in current_page.links():
+			try:
+				print "fetching " + str(title) 
+				possibilities.append(page.Page(title))
+			except Exception, msg:
+				print "caught an error: " + str(msg)
 
-while current_page != destination:
-	possibilities = []
-	for title in current_page.links():
-		try:
-			#print "appending " + str(title) + " to possibilities" 
-			possibilities.append(page.Page(title))
-		except Exception, msg:
-			print "caught an error: " + str(msg)
+		print "computing scores"
+		scores = []
+		for possible_page in possibilities:
+			if possible_page == destination: current_page = possible_page
+			else:
+				score = tf_idf.distance(destination.list_of_words(), possible_page.list_of_words())
+				print possible_page.title() + " gets score " + str(score)
+				scores.append(score)
 
-	print "computing scores"
-	scores = []
-	for possible_page in possibilities:
-		if possible_page == destination: current_page = possible_page
-		else:
-			score = tf_idf.distance(destination.list_of_words(), possible_page.list_of_words())
-			print possible_page.title() + " gets score " + str(score)
-			scores.append(score)
+		best_page = possibilities[scores.index(min(scores))]
+		print "clicking on " + best_page.title()
+		current_page = best_page
 
-	best_page = possibilities[scores.index(min(scores))]
-	print "clicking on " + best_page.title()
-	current_page = best_page
+	print "made it to " + current_page.title()
 
-print "made it to " + current_page.title()
+navigate("Daniel S. Weld", "University of Washington")
