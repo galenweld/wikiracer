ed568600ceaa9d975c31c6db0a0455a93d16928a
Galen Weld <galen.weld@gmail.com>
2015-11-03 13:59:05 -0500

    moved things into helper functions and added some documentation

--- wikiracer.py
+++ wikiracer.py
@@ -2,7 +2,32 @@ import operator
 import page
 import tf_idf
 
+def make_possiblities(current_page):
+	""" Builds a list of pages linked to by current_page """
+	possibilities = []
+	for title in current_page.links():
+		try:
+			print "fetching " + str(title) 
+			possibilities.append(page.Page(title))
+		except Exception, msg:
+			print "caught an error: " + str(msg)
+	return possibilities
+
+
+def compute_scores(possibilities):
+	""" Goes through all possiblites and adds a score field to them. """
+	print "computing scores"
+
+	for possible_page in possibilities:
+		score =	tf_idf.distance(destination.list_of_words(), possible_page.list_of_words())
+		print possible_page.title() + " gets score " + str(score)
+		possible_page.score = score
+
+
 def navigate(origin_title, destination_title):
+	""" Navigates from origin_title to destination_title, trying to find the
+	shortest path. Returns a list of pages visited.
+	At least, it'll do this eventually"""
 	origin = page.Page(origin_title)
 	destination = page.Page(destination_title)
 
@@ -10,21 +35,9 @@ def navigate(origin_title, destination_title):
 	print "starting at " + current_page.title()
 
 	while current_page != destination:
-		possibilities = []
-		for title in current_page.links():
-			try:
-				print "fetching " + str(title) 
-				possibilities.append(page.Page(title))
-			except Exception, msg:
-				print "caught an error: " + str(msg)
+		possibilities = make_possiblities(current_page)
 
-		print "computing scores"
-		for possible_page in possibilities:
-			if possible_page == destination: current_page = possible_page
-			else:
-				score =	tf_idf.distance(destination.list_of_words(), possible_page.list_of_words())
-				print possible_page.title() + " gets score " + str(score)
-				possible_page.score = score
+		compute_scores(possibilities)
 
 		best_yet = possibilities[0]
 		for possible_page in possibilities:
